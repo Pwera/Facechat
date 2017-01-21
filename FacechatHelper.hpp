45b3bce62f7562f5fa94d7a523ef01a6d2dd465f
Pwera <1EThaslo56>
2017-01-21 11:16:58 -0500

    added helper class

---
+++ FacechatHelper.hpp
@@ -0,0 +1,16 @@
+//
+// Created by user on 1/21/17.
+//
+
+#ifndef FACECHAT_FACECHATHELPER_HPP
+#define FACECHAT_FACECHATHELPER_HPP
+
+
+
+class FacechatHelper {
+
+};
+
+
+
+#endif //FACECHAT_FACECHATHELPER_HPP
